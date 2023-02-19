c19511e94fe8c122c4eee3e098c5f9dadb21d20a
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-02-19 17:02:35 +0600

    added js code for get color code

--- JS/style.js
+++ JS/style.js
@@ -2,19 +2,28 @@
 //step-1. create onload handler,
 window.onload =() => {
     main();
-}
+};
 
 function main(){
+    const randomColor = document.getElementById('mixColor');
+    const btn = document.getElementById('change-btn');
+    const output = document.getElementById('output');
 
-}
+    btn.addEventListener('click', function(){
+        const bgColor =  generateHexColor();
+        randomColor.style.backgroundColor = bgColor;
+        output.value = bgColor;
+    });
+};
 
 //step-2. random color generator function,
-function generateRGBColor(){
+function generateHexColor(){
+    // 255, 255, 255, => #ffffff
     const red = Math.floor(Math.random()*255);
     const green = Math.floor(Math.random()*255);
     const blue = Math.floor(Math.random()*255);
 
-    return `rgb(${red}, ${green}, ${blue})`;
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
 };
 
 //step-3. collect all necessary reference,
